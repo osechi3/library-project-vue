2a6678b05344780f61c834fe691e29170a7f745a
osechi3 <holypoetwrk@gmail.com>
2020-08-06 22:12:10 +0300

    Creating Header component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <app-header></app-header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    appHeader: Header
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ body {
+   margin: 0;
+
+   background-color: #E5E5E5;
+ }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="container">
+    <div class="title">Vue Library</div>
+    <button class="btns">Add Book</button>
+    <button class="btns">Add List</button>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+  #container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    height: 60px;
+
+    color: white;
+    font-weight: bold;
+
+    background-color: #14213D;
+    outline: 1px solid;
+  }
+
+  .title {
+    width: 280px;
+
+    font-size: 32px;
+    text-align: center;
+
+    cursor: pointer;
+  }
+
+  .btns {
+    height: inherit;
+    width: 150px;
+
+    font-size: 20px;
+    color: white;
+    font-weight: bold;
+
+    background-color: inherit;
+    border: none;
+    border-left: 2px solid white;
+    border-right: 2px solid white;
+  }
+  .btns:hover {
+    color: #14213D;
+    background-color: #FCA311;
+  }
+</style>
