e86731e54d2d3096f2dd58b1aacfc4f90f0060dc
osechi3 <holypoetwrk@gmail.com>
2020-08-10 09:55:01 +0300

    Checking if there are changes when Confirm button is clicked to decide whether to make a request to the server

--- src/components/Book.vue
+++ src/components/Book.vue
@@ -144,15 +144,19 @@ export default {
     ]),
 
     confirmChanges () {
-      if (this.validate()) {
-        this.changeBook(this.currentBook)
+      if (this.checkObjectEquality(this.currentBook, this.userBooks[this.index]) === false) {
+        if (this.validate()) {
+          this.changeBook(this.currentBook)
+          this.isEditAllowed = !this.isEditAllowed
+        }
+      } else {
         this.isEditAllowed = !this.isEditAllowed
       }
     },
 
     abortChanges () {
+      this.currentBook = Object.assign(this.currentBook, this.userBooks[this.index])
       this.isEditAllowed = !this.isEditAllowed
-      this.currentBook = this.userBooks[this.index]
     },
 
     confirmDeletion () {
@@ -163,6 +167,23 @@ export default {
     validate () {
       this.$v.$touch()
       return !this.$v.$invalid
+    },
+
+    checkObjectEquality (obj1, obj2) {
+      const obj1Keys = Object.keys(obj1)
+      const obj2Keys = Object.keys(obj2)
+
+      if (obj1Keys.length !== obj2Keys.length) return false
+
+      const obj1Values = Object.values(obj1)
+      const obj2Values = Object.values(obj2)
+
+      let numberOfEqualValues = 0
+      obj1Values.forEach(value => {
+        if (obj2Values.find(value2 => value === value2)) numberOfEqualValues++
+      })
+
+      return (numberOfEqualValues === obj1Values.length)
     }
   },
 
